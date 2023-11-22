63229cd905f46b2c29e2ad950a0af21454314590
besky <onlybesky@gmail.com>
2023-11-22 22:15:27 +0900

    feat: 입력 문자열을 `List`로 바꾸는 기능 구현

---
+++ src/main/java/powerball/constant/Format.java
@@ -0,0 +1,23 @@
+package powerball.constant;
+
+public class Format {
+    public static final String BLANK = " ";
+    public static final String EMPTY = "";
+    public static final String INPUT_POWER_BALL_PURCHASE_PRICE = "구입금액을 입력해 주세요.";
+    public static final String INPUT_POWER_PLAY_PURCHASE_PRICE = "파워 플레이 금액을 입력해 주세요.";
+    public static final String BOUGHT_NUMBER = "파워볼 %d개, 파워 플레이 %d번을 구매했습니다.";
+    public static final String BOUGHT_RESULT = "%s, %d, x%d";
+    public static final String INPUT_WHITE_BALL = "화이트볼을 입력해 주세요.";
+    public static final String INPUT_RED_POWER_BALL = "레드 파워볼을 입력해 주세요.";
+    public static final String WIN_STATISTICS = "당첨 통계\n---";
+    public static final String FIRST_PRIZE = "화이트볼 5개 일치, 레드 파워볼 일치 (Grand Prize: 20,000,000달러) - %d개";
+    public static final String SECOND_PRIZE = "화이트볼 5개 일치 (1,000,000달러) - %d개";
+    public static final String THIRD_PRIZE = "화이트볼 4개 일치, 레드 파워볼 일치 (50,000달러) - %d개";
+    public static final String FOURTH_PRIZE = "화이트볼 4개 일치  (100달러) - %d개";
+    public static final String FIFTH_PRIZE = "화이트볼 3개 일치, 레드 파워볼 일치 (100달러) - %d개";
+    public static final String SIXTH_PRIZE = "화이트볼 3개 일치 (7달러) - %d개";
+    public static final String SEVENTH_PRIZE = "화이트볼 2개 일치, 레드 파워볼 일치 (7달러) - %d개";
+    public static final String EIGHTH_PRIZE = "화이트볼 1개 일치, 레드 파워볼 일치 (4달러) - %d개";
+    public static final String NINETH_PRIZE = "레드 파워볼 일치 (4달러) - %d개";
+    public static final String EARNING_RATIO = "총 수익률은 %1f 입니다.";
+}
---
+++ src/main/java/powerball/util/StringUtil.java
@@ -0,0 +1,13 @@
+package powerball.util;
+
+import static powerball.constant.Format.BLANK;
+import static powerball.constant.Format.EMPTY;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class StringUtil {
+    public static List<String> toList(final String string) {
+        return Arrays.stream(string.replace(BLANK, EMPTY).split(",")).toList();
+    }
+}
