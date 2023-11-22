8955d3c7a855194038b3c71a65e3d69fb7754845
besky <onlybesky@gmail.com>
2023-11-22 22:30:04 +0900

    feat: 파워 플레이 참여 여부 저장 기능 구현

--- src/main/java/powerball/domain/dto/BoughtPowerBallsDto.java
+++ src/main/java/powerball/domain/dto/BoughtPowerBallsDto.java
@@ -1,6 +1,7 @@
 package powerball.domain.dto;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Random;
 import powerball.domain.BoughtPowerBall;
@@ -8,13 +9,28 @@ import powerball.domain.BoughtPowerBall;
 public record BoughtPowerBallsDto(
         List<BoughtPowerBall> boughtPowerBalls
 ) {
+    private static final int INITIAL_VALUE = 0;
+
     public static BoughtPowerBallsDto of(final int boughtPowerBallNumber, final int boughtPowerPlayNumber) {
         final List<BoughtPowerBall> balls = new ArrayList<>();
         final Random random = new Random();
-        int currentPowerPlayNumber = 0;
+        final List<Boolean> isPowerPlay = powerPlayOrNot(boughtPowerBallNumber, boughtPowerPlayNumber);
         while (balls.size() != boughtPowerBallNumber) {
-            balls.add(new BoughtPowerBall(true));
+            balls.add(new BoughtPowerBall(isPowerPlay.get(balls.size())));
         }
         return new BoughtPowerBallsDto(balls);
+    }
+
+    private static List<Boolean> powerPlayOrNot(final int boughtPowerBallNumber, int boughtPowerPlayNumber) {
+        final List<Boolean> powerPlay = new ArrayList<>();
+        while (boughtPowerPlayNumber != INITIAL_VALUE) {
+            powerPlay.add(true);
+            boughtPowerPlayNumber--;
+        }
+        while (powerPlay.size() != boughtPowerBallNumber) {
+            powerPlay.add(false);
+        }
+        Collections.shuffle(powerPlay);
+        return powerPlay;
     }
 }
