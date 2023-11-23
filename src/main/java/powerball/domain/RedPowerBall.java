ddad5f5ce81802f52a2820df110a5db2d57176c9
besky <onlybesky@gmail.com>
2023-11-23 19:54:54 +0900

    refactor: 사용하지 않는 클래스 삭제, 레드 파워볼 클래스 수정

--- src/main/java/powerball/domain/RedPowerBall.java
+++ src/main/java/powerball/domain/RedPowerBall.java
@@ -1,45 +1,14 @@
 package powerball.domain;
 
-import static powerball.constant.Ball.INDEX_DIFFERENCE;
 import static powerball.constant.Ball.RED_POWER_BALL;
+import static powerball.util.RandomUtil.createRandomNumber;
 
-import java.util.Random;
-import powerball.exception.NotNumericException;
-import powerball.exception.OutOfRangeException;
+public record RedPowerBall(
+        int number
+) {
 
-public class RedPowerBall {
-    private final int redPowerBall;
-
-    public RedPowerBall(final String number) {
-        validate(number);
-        redPowerBall = Integer.parseInt(number);
-    }
-
-    public RedPowerBall() {
-        redPowerBall = generateRedPowerBall();
-    }
-
-    private void validate(final String number) {
-        try {
-            isRange(Integer.parseInt(number));
-        } catch (NumberFormatException e) {
-            throw new NotNumericException();
-        }
-    }
-
-    private void isRange(final int number) {
-        if (number < RED_POWER_BALL.getMin() || number > RED_POWER_BALL.getMax()) {
-            throw new OutOfRangeException();
-        }
-    }
-
-    private int generateRedPowerBall() {
-        final Random random = new Random();
-        return random.nextInt(RED_POWER_BALL.getMax() + INDEX_DIFFERENCE);
-    }
-
-    public int inquireDetail() {
-        return redPowerBall;
+    public static RedPowerBall createRedPowerBall() {
+        return new RedPowerBall(createRandomNumber(RED_POWER_BALL.getMin(), RED_POWER_BALL.getMax()));
     }
 
 }
---
+++ src/test/java/powerball/PowerballApplicationTests.java
@@ -0,0 +1,13 @@
+package powerball;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PowerballApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
